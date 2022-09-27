27f2c18e2cb312de82112d2248eb156fd86f6398
tkelly1107 <100804786+tkelly1107@users.noreply.github.com>
2022-09-26 23:14:55 -0400

    Delete 200sampledata.R

--- code/200sampledata.R
+++
@@ -1,4 +0,0 @@
-appsover20words <- read.csv("C:\\Users\\recon\\OneDrive - Nova Southeastern University\\Desktop\\MostReviewedAppsOver20Words.csv")
-set.seed(62704)
-sampledata <- appsover20words[sample(nrow(appsover20words), 200), ]
-write.csv(sampledata,"C:\\Users\\recon\\OneDrive - Nova Southeastern University\\Desktop\\Random200Sample.csv", row.names = TRUE)
