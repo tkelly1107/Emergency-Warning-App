08372acf3af3b035197097bd750d001c843f35cf
tkelly1107 <100804786+tkelly1107@users.noreply.github.com>
2022-11-02 02:33:33 -0400

    Add files via upload

---
+++ code/R Code/connect_mongodb_rstudio.r
@@ -0,0 +1,24 @@
+---
+title: "Connecting to Mongo Atlas"
+output: html_notebook
+---
+
+
+library(mongolite)
+connection_string = 'mongodb+srv://admin:MeltemINFP2020@googlecluster.z2zrj.mongodb.net/app_proj_db'
+review_collection = mongo(collection="review_collection", db="app_proj_db", url=connection_string)
+review_collection$iterate()$one()
+alldata <- review_collection$find('{}')
+
+library(dplyr)
+library(textshape)
+library(lexicon)
+library(textclean)
+library(readr)
+library(tidyverse)
+
+filter_empty_row(alldata)
+filter_NA(alldata)
+unique(alldata)
+alldata %>% distinct(reviewId, .keep_all = TRUE)
+
---
+++ code/R Code/remove_collection.r
@@ -0,0 +1,8 @@
+library(mongolite)
+connection_string = 'mongodb+srv://admin:MeltemINFP2020@googlecluster.z2zrj.mongodb.net/app_proj_db'
+review_collection = mongo(collection="review_collection", db="app_proj_db", url=connection_string)
+review_collection$iterate()$one()
+review_collection$remove('{}')
+info_collection = mongo(collection="info_collection", db="app_proj_db", url=connection_string)
+info_collection$iterate()$one()
+info_collection$remove('{}')
